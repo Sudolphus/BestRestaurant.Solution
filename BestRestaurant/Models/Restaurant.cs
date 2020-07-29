43be192906b7f69f93e98d0c9818f0258cc79de7
JohnNils Olson <johnnils@gmail.com>
2020-07-29 14:51:20 -0700

    add address, phone and website properties to restaurant class
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- BestRestaurant/Models/Restaurant.cs
+++ BestRestaurant/Models/Restaurant.cs
@@ -5,6 +5,11 @@ namespace BestRestaurant.Models
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public int CuisineId { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Website { get; set; }
+    public string Phone { get; set;}
     public virtual Cuisine Cuisine { get; set; }
   }
 }
